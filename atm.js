48e86d3d1e213543992cc8bd2751f17dd5f22d55
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2020-02-10 19:21:11 -0300

    map number buttons

--- atm.js
+++ atm.js
@@ -1,29 +1,17 @@
-// Cash Machine
-// O Problema
-// Desenvolva uma aplicação que simule a entrega de notas quando um cliente efetuar um saque em um caixa eletrônico.
+const inputNumbers = [
+  document.querySelector('#n1'),
+  document.querySelector('#n2'),
+  document.querySelector('#n3'),
+  document.querySelector('#n4'),
+  document.querySelector('#n5'),
+  document.querySelector('#n6'),
+  document.querySelector('#n7'),
+  document.querySelector('#n8'),
+  document.querySelector('#n9'),
+  document.querySelector('#n0')
+];
 
-// Sua aplicação deve:
+let mapedInputNumbers = inputNumbers.map((numbers) => {
+  return numbers.addEventListener('click', () => console.log('testando se deu certo'));
+});
 
-// Entregar sempre o menor número de notas possíveis
-// Sacar valores apenas com as notas disponíveis
-// Ter saldo do cliente infinito
-// Ter quantidade de notas infinita
-// Possuir apenas notas de R$ 100,00, R$ 50,00, R$ 20,00 e R$ 10,00
-// Retornar mensagens de erro em caso de entradas inválidas
-// Observação:
-
-// Exemplos:
-// Entrada: 30.00
-// Resultado: [20.00, 10.00]
-
-// Entrada: 80.00
-// Resultado: [50.00, 20.00, 10.00]
-
-// Entrada: 125.00
-// Resultado: Erro de notas indisponíveis
-
-// Entrada: -130.00
-// Resultado: Erro de valor inválido
-
-// Entrada: NULL
-// Resultado: Erro de valor nulo
