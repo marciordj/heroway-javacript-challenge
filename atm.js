ed7afd91b95ae1b4e2c716bb6dcabcdc93dd1e80
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2020-02-10 23:20:55 -0300

    TAMO QUASE LÁ, CONSEGUI PEGAR OS NUMEROS GRAÇAS A DEUS

--- atm.js
+++ atm.js
@@ -1,46 +1,28 @@
-//key numbers
-const inputNumber1 = document.querySelector('div#n1');
-const inputNumber2 = document.querySelector('div#n2');
-const inputNumber3 = document.querySelector('div#n3');
-const inputNumber4 = document.querySelector('div#n4');
-const inputNumber5 = document.querySelector('div#n5');
-const inputNumber6 = document.querySelector('div#n6');
-const inputNumber7 = document.querySelector('div#n7');
-const inputNumber8 = document.querySelector('div#n8');
-const inputNumber9 = document.querySelector('div#n9');
-const inputNumber0 = document.querySelector('div#n0');
-
-inputNumber1.addEventListener('click', inputNumberOnDisplay);
-inputNumber2.addEventListener('click', inputNumberOnDisplay);
-inputNumber3.addEventListener('click', inputNumberOnDisplay);
-inputNumber4.addEventListener('click', inputNumberOnDisplay);
-inputNumber5.addEventListener('click', inputNumberOnDisplay);
-inputNumber6.addEventListener('click', inputNumberOnDisplay);
-inputNumber7.addEventListener('click', inputNumberOnDisplay);
-inputNumber8.addEventListener('click', inputNumberOnDisplay);
-inputNumber9.addEventListener('click', inputNumberOnDisplay);
-inputNumber0.addEventListener('click', inputNumberOnDisplay);
-
-const clearBtn = document.querySelector('div#clear');
-clearBtn.addEventListener('click', clearDisplay);
+const inputNumber = [
+  inputNumber1 = document.querySelector('div#n1'),
+  inputNumber2 = document.querySelector('div#n2'),
+  inputNumber3 = document.querySelector('div#n3'),
+  inputNumber4 = document.querySelector('div#n4'),
+  inputNumber5 = document.querySelector('div#n5'),
+  inputNumber6 = document.querySelector('div#n6'),
+  inputNumber7 = document.querySelector('div#n7'),
+  inputNumber0 = document.querySelector('div#n0'),
+  inputNumber8 = document.querySelector('div#n8'),
+  inputNumber9 = document.querySelector('div#n9'),
+  inputNumber0 = document.querySelector('div#n0')
+];
 
 const displayNumber = document.querySelector('#displayer');
 
-function clearDisplay() {
-  displayNumber.value = '';
+console.dir(inputNumber[1].innerText);
+
+for (let index = 0; index < inputNumber.length; index++) {
+  
+  inputNumber[index].addEventListener('click', () => {
+    let btnNumber = inputNumber[index].innerHTML;
+    console.log(btnNumber);
+    displayNumber.value = btnNumber;
+  })
 }
 
-
-function inputNumberOnDisplay() {
-  switch (displayNumber) {
-    case inputNumber0:
-      displayNumber.value = 0;
-      break;
-    case inputNumber1:
-      displayNumber.value = 1;
-      break;
-    default:
-      console.log('toma no seu cu');
-  }
-}
 
