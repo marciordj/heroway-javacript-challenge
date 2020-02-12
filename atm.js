76b115bed28c667c758ba49cde4ee78370524ab1
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2020-02-12 18:20:41 -0300

    update master branch

--- atm.js
+++ atm.js
@@ -1,25 +1,53 @@
-const inputNumber = [
-  inputNumber1 = document.querySelector('div#n1'),
-  inputNumber2 = document.querySelector('div#n2'),
-  inputNumber3 = document.querySelector('div#n3'),
-  inputNumber4 = document.querySelector('div#n4'),
-  inputNumber5 = document.querySelector('div#n5'),
-  inputNumber6 = document.querySelector('div#n6'),
-  inputNumber7 = document.querySelector('div#n7'),
-  inputNumber0 = document.querySelector('div#n0'),
-  inputNumber8 = document.querySelector('div#n8'),
-  inputNumber9 = document.querySelector('div#n9'),
-  inputNumber0 = document.querySelector('div#n0')
-];
+inputNumber1 = document.querySelector('div#n1')
+inputNumber2 = document.querySelector('div#n2')
+inputNumber3 = document.querySelector('div#n3')
+inputNumber4 = document.querySelector('div#n4')
+inputNumber5 = document.querySelector('div#n5')
+inputNumber6 = document.querySelector('div#n6')
+inputNumber7 = document.querySelector('div#n7')
+inputNumber0 = document.querySelector('div#n0')
+inputNumber8 = document.querySelector('div#n8')
+inputNumber9 = document.querySelector('div#n9')
+inputNumber0 = document.querySelector('div#n0')
 
 const displayNumber = document.querySelector('#displayer');
 
-for (let index = 0; index < inputNumber.length; index++) {
-  
-  inputNumber[index].addEventListener('click', () => {
-    let btnNumber = inputNumber[index].innerHTML;
-    displayNumber.value = btnNumber;
-  })
+
+inputNumber0.addEventListener('click', () => {
+  const btn = inputNumber0.innerText;
+  displayNumber.value = btn;
+});
+
+inputNumber1.addEventListener('click', () => {
+  const btn = inputNumber1.innerText;
+  displayNumber.value = btn;
+} )
+
+
+
+
+
+console.dir(inputNumber0);
+
+function mudarNumero() {
+  const btn = inputNumber0.innerText;
+
+  displayNumber
 }
+
+
+
+
+
+
+
+
+// for (let index = 0; index < inputNumber.length; index++) {
+  
+//   inputNumber[index].addEventListener('click', () => {
+//     let btnNumber = inputNumber[index].innerHTML;
+//     displayNumber.value = btnNumber;
+//   })
+// }
 
 
